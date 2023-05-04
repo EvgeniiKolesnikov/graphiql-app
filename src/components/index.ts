5ca416697fda610afb3d85b9a2f95d06366098ef
Evgenii Kolesnikov <jekow@yandex.ru>
2023-05-04 19:05:06 +0300

    feat: update app structure

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+// --- for --- export const
+// export * from './Component1/Component1';
+// export * from './Component2/Component2';
+
+// --- for -- export default function
+// import Component1 from './Component1/Component1';
+// import Component2 from './Component2/Component2';
+// export { Component1, Component2 };
+
+export * from './App/App';
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+// --- for --- export const
+// export * from './Component1/Component1';
+// export * from './Component2/Component2';
+
+// --- for -- export default function
+// import Component1 from './Component1/Component1';
+// import Component2 from './Component2/Component2';
+// export { Component1, Component2 };
