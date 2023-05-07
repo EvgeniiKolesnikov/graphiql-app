4cd172508f6ad18e69cd38dcbda682010397ffc1
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-07 21:12:43 +0300

    feat: add SignPage

---
+++ src/pages/SignPage/SignPage.tsx
@@ -0,0 +1,10 @@
+import s from './SignPage.module.scss';
+import { SignForm } from 'components/SignForm/SignForm';
+
+export const SignPage = () => {
+  return (
+    <div>
+      <SignForm />
+    </div>
+  );
+};
