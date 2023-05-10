0e98ee4ba22408fb473b107708f01ebdd2f50124
YanaPronko <YaniaR@tut.by>
2023-05-10 14:29:55 +0300

    fix: delete fragment in Router

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -7,16 +7,14 @@ import { MainLayout } from '..';
 
 const Router = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<WelcomePage />} />
-          <Route path="main" element={<MainPage />} />
-          <Route path="404" element={<NotFoundPage />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<WelcomePage />} />
+        <Route path="main" element={<MainPage />} />
+        <Route path="404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Route>
+    </Routes>
   );
 };
 
