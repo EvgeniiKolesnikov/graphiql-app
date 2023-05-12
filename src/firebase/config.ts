93560025ab481e882a456c7c52f33f9cf4447052
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-12 19:46:40 +0300

    feat: add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCxqVzlyRz9J6NBOoHr6c6YwmxGoaSFGHg',
+  authDomain: 'graphiql-app-4af7f.firebaseapp.com',
+  projectId: 'graphiql-app-4af7f',
+  storageBucket: 'graphiql-app-4af7f.appspot.com',
+  messagingSenderId: '812480364456',
+  appId: '1:812480364456:web:dbb2c97bc56c083315679d',
+};
