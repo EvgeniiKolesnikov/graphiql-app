c3b2c1c37977b131f2348cc2fc8582ff31e1e030
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-13 11:48:09 +0300

    feat: update LoginForm component

--- src/components/SignContainer/LoginForm/LoginForm.tsx
+++ src/components/SignContainer/LoginForm/LoginForm.tsx
@@ -1,52 +1,54 @@
-import z from 'zod';
 import { TextInput, PasswordInput, Button } from '@mantine/core';
-import { useForm, zodResolver } from '@mantine/form';
+import { useForm } from '@mantine/form';
+import { auth } from '../../../firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useLogin } from '../../../hooks/auth/useLogin';
+import { Navigate } from 'react-router-dom';
 
 interface IFormValues {
-  login: string;
+  email: string;
   password: string;
 }
 
-const schema = z.object({
-  login: z
-    .string()
-    .min(1, 'Login is required')
-    .min(3, 'Login should have more than 3 characters')
-    .max(15, 'Login should not be longer than 10 characters'),
-  password: z
-    .string()
-    .min(1, 'Password is required')
-    .min(8, 'Password should have more than 8 characters')
-    .max(15, 'Password should not be longer than 15 characters'),
-});
-
 export const LoginForm = () => {
+  const [user] = useAuthState(auth);
+  const { login, isLoading, error } = useLogin();
   const form = useForm({
-    initialValues: { login: '', password: '' },
-    validate: zodResolver(schema),
+    initialValues: { email: '', password: '' },
   });
 
   const onSubmit = (data: IFormValues) => {
-    console.log(data);
+    const { email, password } = data;
+    login(email, password);
   };
 
+  if (user) {
+    return <Navigate to="/main" />;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <form onSubmit={form.onSubmit(onSubmit)}>
-      <TextInput
-        size="xl"
-        placeholder="Your login"
-        label="Login"
-        {...form.getInputProps('login')}
-      />
-      <PasswordInput
-        size="xl"
-        placeholder="Your password"
-        label="Password"
-        {...form.getInputProps('password')}
-      />
-      <Button size="lg" type="submit" fullWidth uppercase variant="outline">
-        Submit
-      </Button>
-    </form>
+    <div>
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          size="xl"
+          placeholder="Your email"
+          label="Email"
+          {...form.getInputProps('email')}
+        />
+        <PasswordInput
+          size="xl"
+          placeholder="Your password"
+          label="Password"
+          {...form.getInputProps('password')}
+        />
+        <Button size="lg" type="submit" fullWidth uppercase variant="outline">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 };
