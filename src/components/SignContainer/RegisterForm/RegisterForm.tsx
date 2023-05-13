54949d9f111f8d3efe36ab091fccf8c0c8a8577a
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-13 11:47:31 +0300

    feat: update RegisterForm component

--- src/components/SignContainer/RegisterForm/RegisterForm.tsx
+++ src/components/SignContainer/RegisterForm/RegisterForm.tsx
@@ -1,6 +1,10 @@
-import z from 'zod';
-import { TextInput, PasswordInput, Button, Container } from '@mantine/core';
+import { TextInput, PasswordInput, Button } from '@mantine/core';
 import { useForm, zodResolver } from '@mantine/form';
+import { schema } from './schema';
+import { auth } from '../../../firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate } from 'react-router-dom';
+import { useRegister } from '../../../hooks/auth/useRegister';
 
 interface IFormValues {
   login: string;
@@ -9,65 +13,58 @@ interface IFormValues {
   email: string;
 }
 
-const schema = z
-  .object({
-    login: z
-      .string()
-      .min(1, 'Login is required')
-      .min(3, 'Login should have more than 3 characters')
-      .max(15, 'Login should not be longer than 10 characters'),
-    password: z
-      .string()
-      .min(1, 'Password is required')
-      .min(8, 'Password should have more than 8 characters')
-      .max(15, 'Password should not be longer than 15 characters'),
-    repeatPassword: z.string().min(1, 'You should repeat your password'),
-    email: z.string().email('Invalid email').min(1, 'Email is required'),
-  })
-  .refine((data) => data.password === data.repeatPassword, {
-    path: ['repeatPassword'],
-    message: 'Passwords do not match',
-  });
-
 export const RegisterForm = () => {
+  const [user] = useAuthState(auth);
+  const { register, isLoading, error } = useRegister();
   const form = useForm({
     initialValues: { login: '', password: '', repeatPassword: '', email: '' },
     validate: zodResolver(schema),
   });
 
   const onSubmit = (data: IFormValues) => {
-    console.log(data);
+    const { login, email, password } = data;
+    register(login, email, password);
   };
 
+  if (user) {
+    return <Navigate to="/main" />;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <form onSubmit={form.onSubmit(onSubmit)}>
-      <TextInput
-        size="xl"
-        placeholder="Your login"
-        label="Login"
-        {...form.getInputProps('login')}
-      />
-      <PasswordInput
-        size="xl"
-        placeholder="Your password"
-        label="Password"
-        {...form.getInputProps('password')}
-      />
-      <PasswordInput
-        size="xl"
-        placeholder="Repeat your password"
-        label="Repeat password"
-        {...form.getInputProps('repeatPassword')}
-      />
-      <TextInput
-        size="xl"
-        placeholder="Your email"
-        label="Email"
-        {...form.getInputProps('email')}
-      />
-      <Button size="lg" type="submit" fullWidth uppercase variant="outline">
-        Submit
-      </Button>
-    </form>
+    <div>
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          size="xl"
+          placeholder="Your login"
+          label="Login"
+          {...form.getInputProps('login')}
+        />
+        <PasswordInput
+          size="xl"
+          placeholder="Your password"
+          label="Password"
+          {...form.getInputProps('password')}
+        />
+        <PasswordInput
+          size="xl"
+          placeholder="Repeat your password"
+          label="Repeat password"
+          {...form.getInputProps('repeatPassword')}
+        />
+        <TextInput
+          size="xl"
+          placeholder="Your email"
+          label="Email"
+          {...form.getInputProps('email')}
+        />
+        <Button size="lg" type="submit" fullWidth uppercase variant="outline">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 };
