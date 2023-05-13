fe9ead61b8bbd0841bda06d5c2458c7185699d66
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-13 11:42:17 +0300

    feat: add useLogin hook

---
+++ src/hooks/auth/useLogin.ts
@@ -0,0 +1,24 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
+import { useState } from 'react';
+
+export const useLogin = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const login = async (email: string, password: string) => {
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      setError('');
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { login, isLoading, error };
+};
