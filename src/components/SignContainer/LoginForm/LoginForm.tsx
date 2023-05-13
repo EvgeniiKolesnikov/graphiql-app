ded898be207bd2c607eac24126d7361d548b8279
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-13 16:04:07 +0300

    feat: update RegisterForm

--- src/components/SignContainer/LoginForm/LoginForm.tsx
+++ src/components/SignContainer/LoginForm/LoginForm.tsx
@@ -1,9 +1,13 @@
-import { TextInput, PasswordInput, Button } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import s from './LoginForm.module.scss';
+import { TextInput, PasswordInput, Button, LoadingOverlay } from '@mantine/core';
+import { useForm, zodResolver } from '@mantine/form';
 import { auth } from '../../../firebase/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useLogin } from '../../../hooks/auth/useLogin';
 import { Navigate } from 'react-router-dom';
+import { schema } from './schema';
+import { Fragment, useEffect } from 'react';
+import { notifications } from '@mantine/notifications';
 
 interface IFormValues {
   email: string;
@@ -15,6 +19,7 @@ export const LoginForm = () => {
   const { login, isLoading, error } = useLogin();
   const form = useForm({
     initialValues: { email: '', password: '' },
+    validate: zodResolver(schema),
   });
 
   const onSubmit = (data: IFormValues) => {
@@ -22,33 +27,46 @@ export const LoginForm = () => {
     login(email, password);
   };
 
+  useEffect(() => {
+    if (error) {
+      notifications.show({
+        title: 'Authentification error',
+        message: error,
+      });
+    }
+  }, [error]);
+
   if (user) {
     return <Navigate to="/main" />;
   }
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div>
-      <form onSubmit={form.onSubmit(onSubmit)}>
-        <TextInput
-          size="xl"
-          placeholder="Your email"
-          label="Email"
-          {...form.getInputProps('email')}
-        />
-        <PasswordInput
-          size="xl"
-          placeholder="Your password"
-          label="Password"
-          {...form.getInputProps('password')}
-        />
-        <Button size="lg" type="submit" fullWidth uppercase variant="outline">
+    <Fragment>
+      <form onSubmit={form.onSubmit(onSubmit)} className={s.form}>
+        <fieldset className={s.inputs}>
+          <TextInput
+            size="xl"
+            placeholder="Your email"
+            label="Email"
+            {...form.getInputProps('email')}
+          />
+          <PasswordInput
+            size="xl"
+            placeholder="Your password"
+            label="Password"
+            {...form.getInputProps('password')}
+          />
+        </fieldset>
+
+        <Button color="grape" size="lg" type="submit" fullWidth uppercase>
           Submit
         </Button>
       </form>
-    </div>
+
+      <LoadingOverlay
+        visible={isLoading}
+        loaderProps={{ size: 'lg', color: 'grape', variant: 'bars' }}
+      />
+    </Fragment>
   );
 };
