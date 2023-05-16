8c04dbbdc1fa800f132bb59df764a85d0c4110f8
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-16 14:02:44 +0300

    feat: add translation for SignContainer

--- src/components/SignContainer/SignContainer.tsx
+++ src/components/SignContainer/SignContainer.tsx
@@ -3,16 +3,18 @@ import { Container } from '@mantine/core';
 import { RegisterForm } from './RegisterForm/RegisterForm';
 import { LoginForm } from './LoginForm/LoginForm';
 import { useLocation, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export const SignContainer = () => {
   const { state } = useLocation();
   const isRegister = state?.isRegister ?? true;
+  const { t } = useTranslation();
 
   return (
     <Container size={'sm'} className={s.container}>
       {isRegister ? <RegisterForm /> : <LoginForm />}
       <Link to={'/sign'} state={{ isRegister: !isRegister }} className={s.link}>
-        {isRegister ? `Already have an account?` : `Don't have an account yet?`}
+        {isRegister ? t(`Already have an account?`) : t(`Don't have an account yet?`)}
       </Link>
     </Container>
   );
