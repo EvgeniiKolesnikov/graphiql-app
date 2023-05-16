4a123a2b51f8dd6da279570bb2732319f4642756
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-16 15:30:00 +0300

    feat: add redirect to landing for MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,13 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase/firebase';
+import { Navigate } from 'react-router-dom';
+
 export const MainPage = () => {
+  const [user] = useAuthState(auth);
+
+  if (!user) {
+    return <Navigate to={'/'} replace />;
+  }
+
   return <h1>EDITORE</h1>;
 };
