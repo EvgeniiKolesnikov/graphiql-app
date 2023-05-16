9a23045e93358f89c73b811df27f35a671012cee
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-16 14:03:47 +0300

    refactor: remove unnecessary imports

--- src/redux/slices/SignSlice.ts
+++ src/redux/slices/SignSlice.ts
@@ -1,6 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { PayloadAction } from '@reduxjs/toolkit';
-import { RootStore } from '../store';
 
 const SignSlice = createSlice({
   name: 'auth',
