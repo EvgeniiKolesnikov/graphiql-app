1396955101957d30957425e001acb4d956105014
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-16 15:22:53 +0300

    feat: protect route /main

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,13 +1,21 @@
 import { MainPage, NotFoundPage, SignPage, WelcomePage } from 'pages';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainLayout } from '..';
+import ProtectedRoute from 'components/ProtectedRoute/ProtectedRoute';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<WelcomePage />} />
-        <Route path="main" element={<MainPage />} />
+        <Route
+          path="main"
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="sign" element={<SignPage />} />
         <Route path="404" element={<NotFoundPage />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
