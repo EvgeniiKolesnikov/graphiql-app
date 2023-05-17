492689e348a74687f00e688a7dea12f7cf968295
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-17 14:38:19 +0300

    feat: add translation to form errors

--- src/components/SignContainer/LoginForm/schema.ts
+++ src/components/SignContainer/LoginForm/schema.ts
@@ -1,6 +1,6 @@
 import z from 'zod';
 
 export const schema = z.object({
-  password: z.string().min(1, 'Password is required'),
-  email: z.string().email('Invalid email').min(1, 'Email is required'),
+  password: z.string().min(1),
+  email: z.string().email().min(1),
 });
