ddb6efb17b63a28cad6d6c492b15b09a378ae268
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-18 20:56:45 +0300

    feat: remove redux store

--- src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-const store = configureStore({
-  reducer: {},
-});
-
-export type RootStore = ReturnType<typeof store.getState>;
-
-export default store;
