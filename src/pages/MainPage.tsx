a5637708be6781ea5813e6be90607c5c0e0289c0
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-18 11:58:18 +0300

    refactor: remove unnecessary redirect

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,3 @@
-import { useAuth } from '../hooks/auth/useAuth';
-import { Navigate } from 'react-router-dom';
-
 export const MainPage = () => {
-  const { user } = useAuth();
-
-  if (!user) {
-    return <Navigate to={'/'} replace />;
-  }
-
   return <h1>EDITORE</h1>;
 };
