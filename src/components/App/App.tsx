a6d2ae765644ab91730f6e6dfb92d667a5e17c51
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-21 22:29:14 +0300

    feat: add ErrorBoundary to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import { Suspense } from 'react';
 import { Spinner } from '..';
 import Router from '../Router/Router';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorPage } from '../../pages/ErrorPage';
 
 export const App = () => {
   return (
-    <Suspense fallback={<Spinner />}>
-      <Router />
-    </Suspense>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <Suspense fallback={<Spinner />}>
+        <Router />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
