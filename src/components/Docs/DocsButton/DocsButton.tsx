57f8360d552ff198c9d4d01f419c659b2ea80c7f
Evgenii Kolesnikov <jekow@yandex.ru>
2023-05-22 21:15:32 +0300

    feat: add DocsButton component

---
+++ src/components/Docs/DocsButton/DocsButton.tsx
@@ -0,0 +1,19 @@
+import s from './DocsButton.module.scss';
+import { Loader } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
+
+interface DocsButtonProps {
+  onClick?: () => void;
+  suspense: boolean;
+}
+
+export const DocsButton = ({ onClick, suspense }: DocsButtonProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <button className={s.button} onClick={onClick}>
+      {suspense ? <Loader color="grape" size="sm" variant="bars" /> : '< '}
+      {t('Docs')}
+    </button>
+  );
+};
