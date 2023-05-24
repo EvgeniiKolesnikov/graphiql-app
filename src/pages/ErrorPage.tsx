3432e2db15b2064b0bceee0eb351bbf7caf0de2c
bp26 <94074742+bp26@users.noreply.github.com>
2023-05-24 20:19:51 +0300

    feat: update ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,15 +1,10 @@
-import { useTranslation } from 'react-i18next';
-import { Flex, Text } from '@mantine/core';
-import error from '../assets/error.svg';
+import { ErrorContainer } from '../components/ErrorContainer/ErrorContainer';
+import { Flex } from '@mantine/core';
 
 export const ErrorPage = () => {
-  const { t } = useTranslation();
   return (
-    <Flex justify={'center'} align={'center'} h={'100%'} direction={'column'}>
-      <img src={error} alt="error image" className="notFound" />
-      <Text color="red" size={'50px'}>
-        {t('Error')}
-      </Text>
+    <Flex align={'center'} justify={'center'} h={'100%'}>
+      <ErrorContainer />
     </Flex>
   );
 };
