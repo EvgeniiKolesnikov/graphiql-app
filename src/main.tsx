723c7db8b2c4533e2c62c2bc6b9a530b8ee39739
YanaPronko <YaniaR@tut.by>
2023-05-27 02:49:31 +0300

    fix: add main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './components';
+import './i18n/i18n';
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+
+import './styles/index.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <MantineProvider withGlobalStyles theme={{ fontFamily: 'Roboto, sans-serif' }}>
+        <Notifications />
+        <App />
+      </MantineProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
